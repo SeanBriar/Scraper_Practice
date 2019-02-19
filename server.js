7c00c05a8af36e6c994307475cc2d63305af020c
Sean <seanoneill@Bons-MacBook-Pro.local>
2019-02-19 15:42:38 -0800

    psuedo coded basics

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
 
+// Holds data that is being scraped
 let results = [];
-// Make a request via axios to grab the HTML body from the site of your choice
+
+// Make a request via axios to grab the HTML elements
 axios.get("http://feministing.com/").then(function(response) {
 
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+  // Load the HTML into cheerio and save it to a variable ($ for cheerio)
   let $ = cheerio.load(response.data);
 
-  // An empty array to save the data that we'll scrape
 
-  // Select each element in the HTML body from which you want information.
-  // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-  // but be sure to visit the package's npm page to see how it works
+  // Select each element in the HTML body
   $("div.twoCols").children("article").each(function(i, element) {
 
     let title = $(element).children("header").find("a").text();
     let link = $(element).find("a").attr("href");
     let content = $(element).children().find("div.show-for-small-only").text()
 
-    // Save these results in an object that we'll push into the results array we defined earlier
+    // Save these results in an object by pushing to results array
     results.push({
       title: title,
       link: link,
@@ -28,8 +26,8 @@ axios.get("http://feministing.com/").then(function(response) {
     });
   });
 
-  // Log the results once you've looped through each of the elements found with cheerio
+  //Convert data to JSON object
   let resultsJSON = JSON.stringify(results, null, 2)
-  // resultsJSON.pretty()
+
   console.log(resultsJSON);
 });
