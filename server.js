f101c4b25cf8066f8c5214988fd6f10a0f0aedd0
Sean <seanoneill@Bons-MacBook-Pro.local>
2019-02-19 17:09:11 -0800

    multiple page scrapes

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 
 // Holds data that is being scraped
-let results = [];
+let results = []
+let products = []
 
 // Make a request via axios to grab the HTML elements
-axios.get("http://feministing.com/").then(function(response) {
-
+//Main page scrape
+axios.get("http://feministing.com/").then((response) => {
+  let finalObj = {}
   // Load the HTML into cheerio and save it to a variable ($ for cheerio)
   let $ = cheerio.load(response.data);
 
@@ -27,7 +29,27 @@ axios.get("http://feministing.com/").then(function(response) {
   });
 
   //Convert data to JSON object
-  let resultsJSON = JSON.stringify(results, null, 2)
 
+  let resultsJSON = JSON.stringify(results, null, 2)
   console.log(resultsJSON);
+
+
+  //Secondary page scrape
+  axios.get("https://www.cafepress.com/feministingstore/8351380").then((response)=>{
+    let $ = cheerio.load(response.data)
+
+    $("td").children("div.shopResultImage").each((i, element)=>{
+      let productLink = $(element).find("a").attr("href")
+      let productImage = $(element).find("img").attr("src")
+
+      products.push({
+        productLink: productLink,
+        productImage: productImage
+      })
+    })
+
+    let productsJSON = JSON.stringify(products, null, 2)
+    console.log(productsJSON)
+
+  })
 });
